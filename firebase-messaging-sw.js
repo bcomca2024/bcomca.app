ce7f9fd87d43a338556fbdba8885c028842c3ec6
bcomca2024 <sgac.bcomca.2025@gmail.com>
2025-07-20 15:48:02 +0530

    ss

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+// This must be at the root of your 'web/' folder
+
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging-compat.js');
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBGe4AxqLzustb0pCYOyIULDR2Ls819NWY",
+  authDomain: "sgac-student-hub.firebaseapp.com",
+  projectId: "sgac-student-hub",
+  storageBucket: "sgac-student-hub.appspot.com",
+  messagingSenderId: "127886153976",
+  appId: "1:127886153976:web:fdfd22f26741b8603d030d",
+});
+
+// Retrieve messaging instance
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icons/icon-192.png' // optional icon path
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
